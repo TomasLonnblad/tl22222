7190741e73eb9d16ba083ddb399e3de4ea98466c
TomasLonnblad <tomaslonnblad@gmail.com>
2025-12-25 16:07:04 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="
+      w-full
+      overflow-hidden
+      overflow-y-auto"
+    >
+      Home
+    </div>
+  )
+}
